9b9fbab0d841d746ef880e0f1b996c8771a9ca71
Mathys <mathyswl10@gmail.com>
2026-04-24 21:29:57 +0200

    commit

--- icons.js
+++ icons.js
@@ -1,7 +1,8 @@
+
 const Icons = {
 
   user: `
-    <svg class="icon-svg" viewBox="0 0 24 24" fill="currentColor">
+    <svg class="icon-svg" viewBox="0 0 24 24" width="18" height="18" fill="currentColor">
       <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 
       1.79-4 4 1.79 4 4 4zm0 2c-4.42 0-8 
       2.24-8 5v1h16v-1c0-2.76-3.58-5-8-5z"/>
@@ -9,7 +10,7 @@ const Icons = {
   `,
 
   logout: `
-    <svg class="icon-svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+    <svg class="icon-svg" viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" stroke-width="2">
       <path d="M10 17l5-5-5-5"/>
       <path d="M15 12H3"/>
       <path d="M21 21V3"/>
