242a2c2c91fcb7a842a8eeb6f6ef0fac30a1ef99
Mathys <mathyswl10@gmail.com>
2026-04-26 14:31:14 +0200

    commit

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,12 +1,23 @@
 import { auth } from "../firebase/firebase.config.js";
 import { GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const provider = new GoogleAuthProvider();
+// const provider = new GoogleAuthProvider();
+
+// export async function loginWithGoogle() {
+//   try {
+//     await signInWithPopup(auth, provider);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
 
 export async function loginWithGoogle() {
   try {
-    await signInWithPopup(auth, provider);
+    console.log("Tentative login Google...");
+    const result = await signInWithPopup(auth, provider);
+    console.log("User connecté :", result.user);
   } catch (error) {
-    console.error(error);
+    console.error("Erreur login Google :", error.code, error.message);
   }
 }
